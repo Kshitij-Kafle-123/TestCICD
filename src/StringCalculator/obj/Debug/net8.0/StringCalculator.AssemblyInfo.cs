743abab6cacfbec661845e44d6bff98d239e24b7
Kshitij Kafle <kshitij.kafle@omnicommediagroup.com>
2025-11-11 12:43:12 +0545

    test

--- src/StringCalculator/obj/Debug/net8.0/StringCalculator.AssemblyInfo.cs
+++ src/StringCalculator/obj/Debug/net8.0/StringCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cc2779307b6ad812c2f6b0297847f812c0fe4d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3272d9912928ccfdb01bc723cba4286666c87d55")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
